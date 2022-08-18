723d556c3c3856c6f3d58761a45a8eac0957f57c
Bastrijan Ali <bastrijan.ali@antiloop.com>
2022-08-18 17:13:42 +0200

    delete_file_name_zadaci_in_urejanje_php_file

--- televizor.php
+++ televizor.php
@@ -2,25 +2,16 @@
 class Televizor{
 	
 	private $Philips = 'slika';
-	 //private $Samsung = 'zvok';
 
 	 //getting da se pridobi ali da se zema vrednost object
 	public function getName() {
 		return $this->Philips;
 	}
 
-	// public function getName1() {
-	// 	return $this->Samsung;
-	// } 
-
 	 //setting, da se dade vrednost, postavi, nastavi, definira katero vrednost naj dobi object 
 	public function setName($newName){
 	 	$this->Philips = $newName;
 	 }
-
-	 // public function setName1($novoIme) {
-	 // 	$this->Samsung = $novoIme;
-	 // }
 	 
 }	
 
@@ -31,13 +22,6 @@ echo 'Namestiv ' . $object -> getName();
 
 $object->setName ('daljinsko');
 echo ' z ' . $object->getName();
-
-
-// $objectSamsung = new Televizor();
-// echo 'Z ' . $objectSamsung -> getName1();
-
-// $objectSamsung setName1 ('hdmi');
-// echo ' namestiv ' . $objectSamsung -> getName1;
 
 ?>
 
